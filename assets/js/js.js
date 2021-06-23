93549a8d1634928fd50214a5c48772f1e7a94b8f
vcrespolozano <vcrespolozano@gmail.com>
2021-06-23 15:35:34 +0200

    Añadido botón para subir arriba con animación y efectos. Añadida primera versión de contenido con solo texto.

--- assets/js/js.js
+++ assets/js/js.js
@@ -1,4 +1,5 @@
 const ancho_pantalla = $(window).width();
+const alto_pantalla  = $(window).height();
 const year           = new Date().getFullYear();
 const alto_cabecera  = $('header').outerHeight(true);
 
@@ -58,6 +59,16 @@ $(document).ready(function(){
         $('.menu').removeClass('on');
     }
 
+    // SUBIR AL TOP DE LA PÁGINA DESDE EL PIE
+    $('.btn_top').click(function() { 
+        $('html, body').animate({scrollTop:0}, 1000);
+        return false;
+    });
+
+    // ALTO DE LOS CONTENIDOS INTERIORES
+    if( $('.text_content').length == 1 || $('.new_content').length == 1 )
+        alto_inner_content();
+
 });
 
 $(window).on('load', function(){ 
@@ -68,16 +79,25 @@ $(window).on('load', function(){
     altos_mixtos();
 });
 
+let lastScrollTop = 0;
 $(window).scroll(function (event) {
-    // GESTIÓN DE OPCIONES DEL MENÚ
-    let scroll = $(window).scrollTop();
-    let opcion = '';
 
-    // HSCROLL
+    let scroll         = $(window).scrollTop();
     let max_scroll     = $(document).height() - $(window).height();
     let current_scroll = $(window).scrollTop();
-    let perc_fill      = (current_scroll * 100) / max_scroll;
-    $('.hscroll').css('width', perc_fill + '%');
+    let perc_scroll    = (current_scroll * 100) / max_scroll;
+
+    // Para mostrar el botón de subir al top
+    if( perc_scroll > 25 )
+        $('.btn_top').addClass('on');
+    else
+        $('.btn_top').removeClass('on');
+
+    // Efecto de rellenado con scroll del subir top
+    let perc_fill      = 100 - perc_scroll;
+    $('.fill').css('top', perc_fill + '%');
+
+    lastScrollTop = scroll;
 });
 
 $(window).on('orientationchange', function(){
@@ -107,6 +127,16 @@ const altos_mixtos = () => {
             $('.misc .fullwidth img').css('height', 'auto');
         }
     }
+}
+
+const alto_inner_content = () => {
+    let alto_pie         = $('footer').outerHeight(true);
+    let min_alto_content = alto_pantalla - alto_cabecera - alto_pie;
+
+    if( $('.text_content').length == 1 )
+        $('.text_content').css('min-height', min_alto_content+'px');
+    else if( $('.new_content').length == 1 )
+        $('.new_content').css('min-height', min_alto_content+'px');
 }
 
 const validarContacto = () => {
